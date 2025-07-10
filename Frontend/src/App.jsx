6800b497aa6d63e34de3ef26a3e8d374d26a4555
usman turk <110340146+Farman1304@users.noreply.github.com>
2025-07-10 21:46:31 +0500

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import SignUpForm from "./components/SignUpForm"
+import SignInForm from "./components/SignInForm"
 
 function App() {
 
 
   return (
-    <>
-      <SignUpForm />
-    </>
+  <Routes>
+    <Route path="/" element={<SignUpForm />} />
+    <Route path="/signIn" element={<SignInForm />} />
+  </Routes>
   )
 }
 
